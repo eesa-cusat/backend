9557d9f3bcbd85498ebe7b90e4c28fafd07ff979
Afsalkalladi <afsalkalladi@gmail.com>
2025-07-20 16:18:47 +0530

    cors fix

--- eesa_backend/settings.py
+++ eesa_backend/settings.py
@@ -207,11 +207,12 @@ REST_FRAMEWORK = {
 }
 
 # CORS settings
+import os
+
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    origin.strip() for origin in os.environ.get('CORS_ALLOWED_ORIGINS',
+        'http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,http://127.0.0.1:5173'
+    ).split(',') if origin.strip()
 ]
 
 CORS_ALLOW_CREDENTIALS = True
